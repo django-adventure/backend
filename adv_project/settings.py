e2094a3e3246816177b03439417a648b0cd70868
allisonkydy <allisnelly@gmail.com>
2020-02-03 12:14:45 -0600

    comment out session auth setting

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -127,7 +127,7 @@ REST_FRAMEWORK = {
     # ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
 }
